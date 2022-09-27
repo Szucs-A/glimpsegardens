5e920b0fa0ae8df0e761394c1623d55c6699cbcc
Szucs-A <aaron@aaronszucs.ca>
2022-09-27 18:47:17 -0400

    Big Camera/Image Picker Problem + Java Problems.

--- glimpsegardens/android/app/src/main/kotlin/com/example/glimpsegardens/MainActivity.kt
+++ glimpsegardens/android/app/src/main/kotlin/com/example/glimpsegardens/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.glimpsegardens
+package com.glimpsegardens.social
 
 import io.flutter.embedding.android.FlutterActivity
 
--- glimpsegardens/lib/main.dart
+++ glimpsegardens/lib/main.dart
@@ -8,7 +8,7 @@ import 'package:video_player/video_player.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:geocoder/geocoder.dart';
 import 'package:email_validator/email_validator.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:image_picker/image_picker.dart'; TODO:
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -44,10 +44,23 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({Key key}) : super(key: key);
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome to Flutter',
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
